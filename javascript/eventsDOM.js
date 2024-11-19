dd9491f3ae3196bd83a8a952a5271b4cea5dae68
Ritzz <alexisrincon27@outlook.com>
2024-11-18 21:25:24 -0500

    events

---
+++ javascript/eventsDOM.js
@@ -0,0 +1,27 @@
+// ? JSON -> JavaScript Object Notation
+// ? XML --- AJAX -> Asyncronous Javascript and XML
+
+// ? JSONArray === [{}]
+// ? JSONObject === {}
+
+let todo = [];
+
+// let todo = [
+// 	{
+// 		// ! SYNTAX {PROPIEDAD: VALOR}
+// 		task: "No apostarle a colombia",
+// 		done: false,
+// 	},
+// 	{
+// 		task: "Comprar las cervezas",
+// 		done: true,
+// 	},
+// ];
+
+let addbtn = document.getElementById("add-button");
+
+addbtn.addEventListener("click", (evt) => {
+  console.log("presionaste" + evt);
+  
+
+});
