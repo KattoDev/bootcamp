ae889dbae37b5c0cfe149cf8707ea52d3e0ca668
Ritzz <alexisrincon27@outlook.com>
2024-11-20 21:17:44 -0500

    add events

--- javascript/eventsDOM.js
+++ javascript/eventsDOM.js
@@ -4,24 +4,52 @@
 // ? JSONArray === [{}]
 // ? JSONObject === {}
 
-let todo = [];
+let todos = [
+	{
+		// ! SYNTAX {KEY: VALUE}
+		task: "No apostarle a colombia",
+		done: false,
+	},
+	{
+		task: "Comprar las cervezas",
+		done: true,
+	},
+];
 
-// let todo = [
-// 	{
-// 		// ! SYNTAX {PROPIEDAD: VALOR}
-// 		task: "No apostarle a colombia",
-// 		done: false,
-// 	},
-// 	{
-// 		task: "Comprar las cervezas",
-// 		done: true,
-// 	},
-// ];
+const addbtn = document.getElementById("add-button");
 
-let addbtn = document.getElementById("add-button");
+const taskinput = document.querySelector("form input");
+
+const container = document.querySelector(".task-container");
+
+container.addEventListener("click", (evt) => {
+	const elm = evt.target;
+
+	if (elm.classList.contains("task")) {
+		console.log("este es", elm.dataset.index);
+	}
+});
 
 addbtn.addEventListener("click", (evt) => {
-  console.log("presionaste" + evt);
-  
+	const input = taskinput.value.trim();
 
+	if (input !== "") {
+		const newTask = { task: input, done: false };
+		taskinput.value = "";
+		todos.push(newTask);
+		console.log(todos.length);
+		// create the task template
+		const taskTemplate = `
+			<div class="task" data-index="${todos.length - 1}">
+				<h3>${newTask.task}</h3>
+				<div class="buttons-container">
+					<input type="checkbox" name="task-done" id="task-done" />
+					<button>Eliminar</button>
+				</div>
+			</div>
+		`;
+
+		// inject the HTML template
+		container.insertAdjacentHTML("beforeend", taskTemplate);
+	}
 });
